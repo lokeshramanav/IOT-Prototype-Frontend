ac846ea6fa4be4e3c394abd84d640bdce631328a
lokeshramanav <lokeshmay3@gmail.com>
2020-06-01 21:03:26 +0800

    SLot Booking

--- src/api/api_helper.js
+++ src/api/api_helper.js
@@ -11,3 +11,47 @@ export const getMalls = ()=>{
             console.log( "there is an error in mall api: " + err);
         })
 }
+
+
+export const getSlots = (startDate, startTime , mallName, userName )=>{
+
+    return fetch (`${API}/slot/getSlots`, {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            startDate: startDate,
+            startTime: startTime,
+            mallName: mallName,
+            userName: userName
+        })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log( "there is an error in get slots api: " + err);
+        })
+}
+
+
+
+
+export const createCategory =( userId, token, category ) =>{
+    return fetch(`/api/category/create/${userId}`, {
+      method: "POST",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization:`Bearer ${token}`
+      },
+      body: JSON.stringify(category)
+  })
+      .then(response => {
+          return response.json();
+      })
+      .catch(err => {
+          console.log( "there is an error in Category: " + err);
+      });
+  } ;
--- src/components/bookslot.js
+++ src/components/bookslot.js
@@ -7,13 +7,19 @@ import TimePicker from 'rc-time-picker';
 import 'rc-time-picker/assets/index.css';
 import moment from 'moment';
 import { useCookies } from 'react-cookie';
-import {getMalls} from '../api/api_helper';
+import {getMalls , getSlots} from '../api/api_helper';
 
 
 const BookSlot = ()=>{
     const [cookies, setCookie] = useCookies(['name']);
     const[bookingDate,setBookingDate ] = React.useState(new Date())
     const[malls,setMalls] = React.useState([])
+    const[mall,setMall] = React.useState("")
+    const[bookingTime,setBookingTime] = React.useState(moment())
+    const[bookingMessage, setBookingMessage] = React.useState({
+        showMessage: false,
+        message: ""
+    })
 
     const init = () => {
         getMalls().then(data => {
@@ -27,44 +33,76 @@ const BookSlot = ()=>{
 
     React.useEffect(() => {init();}, []);
 
-
+    const handleMallChange = (data)=>{
+        console.log("This is the mall!!!")
+        console.log(data)
+        setMall(data)
+    }
     const handlebookingDate = (date)=>{
         console.log(date)
         setBookingDate(date)
     }
 
+    const handlebookingTime = (time)=>{
+        console.log(time)
+        setBookingTime(time)
+    }
+
+    const printDetails = (e)=>{
+        e.preventDefault();
+        getSlots(bookingDate.toISOString().slice(0,10), bookingTime.format("HH:mm"), mall, cookies.name ).then(data=>{
+            console.log("this is the response for slots")
+            console.log(data)
+            setBookingMessage({...bookingMessage,message:data.message,showMessage:true})
+        })
+    }
+
     const bookingSlotForm = ()=>{
         return (
-            <div className="login-clean-bs" style={{height:"1000px", backgroundColor: "rgb(135,177,213)"}}>
-            <form >
-
-            
-            <h2 >Hello {cookies.name}!!!</h2>
-            <div style={{margin:"25px"}}></div>
-            <div class="form-group">
-            <label>Select Date: </label>
-            <DatePicker selected={bookingDate} onChange={(date)=> handlebookingDate(date)}/>
-            <div style={{margin:"25px"}}></div>
-            <label>Select Time: </label>
-            <TimePicker defaultValue={moment()} showSecond={false} minuteStep={15} />
-            <div style={{margin:"25px"}}></div>
-            <label>Select Mall: &nbsp;&nbsp;</label>
-            <select>
-            <option>Please select</option>
-            {malls &&
-                malls.map((c, i) => (
-                    <option key={i} value={c.id}>
-                        {c.mall_name}
-                    </option>
-                ))}
-            </select>
-            </div>
-            <div>
-                    <textarea style={{height:"250px"}}></textarea>
-            </div>
-            </form>
-            </div>
+            <div className="login-clean-bs" style={{height:"1000px" ,backgroundColor: "rgb(135,177,213)"}}>
+  <form>
+    <h2 ><b><i>Hello {cookies.name}!!!</i></b></h2>
+    <div style={{margin:"50px"}}></div>
+    <div class="form-group">
+      <div class="input-group date " id="startDate">
+        <label><b>Select Date:&nbsp;&nbsp;</b></label>
+        <DatePicker selected={bookingDate} onChange={(date)=> handlebookingDate(date)}/>
+      </div>
+      <div class="input-group date" id="startTime">
+        <label><b>Select Time: &nbsp;&nbsp;</b></label>
+        <TimePicker value={bookingTime} onChange={(value)=>{handlebookingTime(value)}} showSecond={false} minuteStep={15} />
+      </div>
+      <div class="input-group date" id="startTime">
+      <label><b>Select Mall:&nbsp;&nbsp;</b></label>
+        <select onChange={(event)=>{handleMallChange(event.target.value)}}>
+        <option>Please select</option>
+        {malls &&
+            malls.map((c, i) => (
+                <option key={i} value={c.mall_name}>
+                    {c.mall_name}
+                </option>
+            ))}
+        </select>
+      </div>
+      <div class="input-group date" id="startTime">
+      <button className="btn-block" onClick={e=>printDetails(e)} style={{color: "rgba(0,0,0,0.61)",fontFamily: "Alfa Slab One, cursive",fontSize: "16px"}}>BOOK</button>
+      </div>
+    </div>
+  </form>
+  {slotResults(bookingMessage)}
+</div>
         );
+    }
+
+    const slotResults=(bookingMessage)=>{
+
+        if(bookingMessage.showMessage){
+        return(<div>
+            <div style={{width:"25px"}}></div>
+        <form style={{marginTop:"10px" , paddingTop:"10px"}}>
+      <h2 >{bookingMessage.message}</h2>
+      </form></div>);}
+
     }
 
 
